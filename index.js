a56bb77c0dbd44778aee5d16d802947afad3ba39
祭り実行委員会 <yturhf24@gmail.com>
2026-05-28 20:59:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
@@ -12,5 +11,4 @@ app.listen(PORT, () => {
   console.log('Web Server Start');
 });
 
-// Discord起動
 require('./bot');
