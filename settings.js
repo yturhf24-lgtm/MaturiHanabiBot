2ab6fb327bfa23e1f860c7fc4b7cf5b8b16babcc
祭り実行委員会 <yturhf24@gmail.com>
2026-05-28 21:58:02 +0900

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,25 +1,24 @@
 const fs = require('fs');
 
-const FILE = './settings.json';
-
-const defaultData = {
-  monitorEnabled: true,
-  linkAlertEnabled: true,
-  newAccountAlertEnabled: true,
-  alertChannelId: null,
-  allowedRoleIds: [],
-  panelText: '説明を入力してください'
-};
+const path = './settings.json';
 
 function load() {
-  if (!fs.existsSync(FILE)) {
-    fs.writeFileSync(FILE, JSON.stringify(defaultData, null, 2));
+  if (!fs.existsSync(path)) {
+    fs.writeFileSync(path, JSON.stringify({
+      alertChannelId: null,
+      linkAlertEnabled: false,
+      playerMonitorEnabled: false,
+      allowedRoles: [],
+      panelText: "",
+      panelChannelId: null
+    }, null, 2));
   }
-  return JSON.parse(fs.readFileSync(FILE, 'utf8'));
+
+  return JSON.parse(fs.readFileSync(path));
 }
 
 function save(data) {
-  fs.writeFileSync(FILE, JSON.stringify(data, null, 2));
+  fs.writeFileSync(path, JSON.stringify(data, null, 2));
 }
 
 module.exports = { load, save };
