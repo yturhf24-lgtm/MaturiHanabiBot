2d073220b51022648068baed996b03000e854448
祭り実行委員会 <yturhf24@gmail.com>
2026-05-29 23:56:04 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+js id="full_auto_register_index"
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
@@ -5,11 +6,13 @@ const path = require("path");
 const {
   Client,
   GatewayIntentBits,
-  Collection
+  Collection,
+  REST,
+  Routes
 } = require("discord.js");
 
 // =====================
-// TOKEN
+// TOKENだけ入れる
 // =====================
 const TOKEN = "MTM1MzM5MzE5NDQxNDYzNzE5OA.GdeWGI.JTZzWSofzKmx8eGepOQ_tY1Xw4RniNj4YXOv2s";
 
@@ -25,7 +28,7 @@ function getClientId(token) {
 const CLIENT_ID = getClientId(TOKEN);
 
 // =====================
-// Express
+// Express（Render用）
 // =====================
 const app = express();
 
@@ -133,13 +136,53 @@ client.on(
 // =====================
 // 起動
 // =====================
-client.once("ready", () => {
+client.once("ready", async () => {
 
   console.log("=================");
   console.log(
     `🤖 ${client.user.tag}`
   );
   console.log("=================");
+
+  // =====================
+  // SlashCommand自動保存
+  // =====================
+  try {
+
+    const commands = [];
+
+    client.commands.forEach(command => {
+
+      commands.push(
+        command.data.toJSON()
+      );
+
+    });
+
+    const rest = new REST({
+      version: "10"
+    }).setToken(TOKEN);
+
+    // 完全上書き保存
+    await rest.put(
+
+      Routes.applicationCommands(
+        CLIENT_ID
+      ),
+
+      {
+        body: commands
+      }
+
+    );
+
+    console.log("✅ Slash Commands Updated");
+
+  } catch (err) {
+
+    console.error(err);
+
+  }
 
 });
 
