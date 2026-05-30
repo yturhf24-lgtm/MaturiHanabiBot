a049b82dd78243aeb646adce7c26cd69dc08a1eb
祭り実行委員会 <yturhf24@gmail.com>
2026-05-30 09:49:42 +0900

    Update checkAdmin.js

--- commands/utils/checkAdmin.js
+++ commands/utils/checkAdmin.js
@@ -2,12 +2,9 @@ const {
   PermissionFlagsBits
 } = require("discord.js");
 
-const OWNER_ID =
-  "1266013271518089258";
+const OWNER_ID = "1266013271518089258";
 
-function checkAdmin(
-  interaction
-) {
+function checkAdmin(interaction) {
 
   const isAdmin =
     interaction.member.permissions.has(
@@ -15,14 +12,9 @@ function checkAdmin(
     );
 
   const isOwner =
-    interaction.user.id ===
-    OWNER_ID;
+    interaction.user.id === OWNER_ID;
 
-  return (
-    isAdmin ||
-    isOwner
-  );
-
+  return isAdmin || isOwner;
 }
 
 module.exports = {
