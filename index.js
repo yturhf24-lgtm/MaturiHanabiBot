c952b6a7a3408c52a4f2c5bac6b64275320858d1
祭り実行委員会 <yturhf24@gmail.com>
2026-05-31 21:01:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,16 @@
-require("dotenv").config();
-require("./keepalive");
-
-const fs = require("fs");
 const {
     Client,
-    Collection,
     GatewayIntentBits
-} = require("discord.js");
+} = require('discord.js');
 
 const client = new Client({
     intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildPresences
+        GatewayIntentBits.Guilds
     ]
 });
 
-client.commands = new Collection();
-
-const commandFiles = fs
-    .readdirSync("./commands")
-    .filter(file => file.endsWith(".js"));
-
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.data.name, command);
-}
-
-const eventFiles = fs
-    .readdirSync("./events")
-    .filter(file => file.endsWith(".js"));
-
-for (const file of eventFiles) {
-    const event = require(`./events/${file}`);
-
-    if (event.once) {
-        client.once(event.name, (...args) =>
-            event.execute(...args)
-        );
-    } else {
-        client.on(event.name, (...args) =>
-            event.execute(...args)
-        );
-    }
-}
+client.once('ready', () => {
+    console.log(`${client.user.tag} 起動完了`);
+});
 
 client.login(process.env.TOKEN);
