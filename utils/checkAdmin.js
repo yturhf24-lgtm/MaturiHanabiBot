5bf767a735abb1d68bf094c7add2417cf202bef5
祭り実行委員会 <yturhf24@gmail.com>
2026-05-31 14:45:18 +0900

    Update checkAdmin.js

--- utils/checkAdmin.js
+++ utils/checkAdmin.js
@@ -1,16 +1,14 @@
-const {
-  PermissionFlagsBits
-} = require("discord.js");
-
-const OWNER_ID = "1266013271518089258";
-
 function checkAdmin(interaction) {
 
-  return (
-    interaction.member.permissions.has(
-      PermissionFlagsBits.Administrator
-    ) ||
-    interaction.user.id === OWNER_ID
+  if (
+    interaction.user.id ===
+    "1266013271518089258"
+  ) {
+    return true;
+  }
+
+  return interaction.member.permissions.has(
+    "Administrator"
   );
 
 }
