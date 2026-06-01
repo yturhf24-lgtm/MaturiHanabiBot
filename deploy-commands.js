c55cb4cfcd995d2aa9bca1c381857e9d93783215
祭り実行委員会 <yturhf24@gmail.com>
2026-06-01 22:50:30 +0900

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,1 +1,45 @@
+require("dotenv").config();
 
+const fs = require("fs");
+const path = require("path");
+
+const { REST, Routes } = require("discord.js");
+
+const commands = [];
+
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter(file =>
+    file.endsWith(".js")
+);
+
+for (const file of commandFiles) {
+
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: "10" })
+    .setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+
+        console.log("コマンド登録開始");
+
+        await rest.put(
+            Routes.applicationCommands(
+                process.env.CLIENT_ID
+            ),
+            {
+                body: commands
+            }
+        );
+
+        console.log("コマンド登録完了");
+
+    } catch (error) {
+        console.error(error);
+    }
+})();
