66cd860e8d6143a2a56e91a2c4f1b35bd1935c4b
祭り実行委員会 <yturhf24@gmail.com>
2026-06-03 11:38:06 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+client.commands = new Collection();
+
+const commands = [];
+
+const commandsPath = path.join(
+    __dirname,
+    "commands"
+);
+
+const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter(file =>
+        file.endsWith(".js")
+    );
+
 for (const file of commandFiles) {
 
     try {
