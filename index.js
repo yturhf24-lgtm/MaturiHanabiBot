e9e637d5ad5231233e9973bcea548e2bc45219c9
祭り実行委員会 <yturhf24@gmail.com>
2026-06-12 00:51:41 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { execSync, spawn } = require("child_process");
 
 // Pythonの依存関係をインストール
 console.log("Installing Python dependencies...");
-execSync("pip install -r requirements.txt", { stdio: "inherit" });
+execSync("pip install -r requirements.txt --break-system-packages", { stdio: "inherit" });
 
 // main.pyを起動
 console.log("Starting bot...");
