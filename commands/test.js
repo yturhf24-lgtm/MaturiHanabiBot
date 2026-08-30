b8b2d0bd98e5a5af95a44c93592a94e0f79d7ccd
祭り実行委員会 <yturhf24@gmail.com>
2026-08-30 18:13:05 +0900

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -3,7 +3,8 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 module.exports = {
   data: new SlashCommandBuilder().setName('test').setDescription('動作確認'),
   async execute(i) {
-    await i.reply({
+    await i.deferReply({ ephemeral: true }); // 🔒 自分だけに表示
+    await i.editReply({
       embeds: [new EmbedBuilder().setColor('Green').setTitle('✅ 成功！').setDescription('Botは正常に動いています！')]
     });
   }
