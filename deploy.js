8ead01794a2d4f94e502be820d424655c4ac031e
祭り実行委員会 <yturhf24@gmail.com>
2026-08-30 17:38:48 +0900

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -6,7 +6,7 @@ for (const f of fs.readdirSync('./commands').filter(f => f.endsWith('.js'))) {
   cmds.push(require(`./commands/${f}`).data.toJSON());
 }
 
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
-rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: cmds })
+new REST({version:'10'}).setToken(process.env.TOKEN)
+.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), {body: cmds})
 .then(() => console.log('✅ コマンド登録完了'))
 .catch(e => console.error('❌ エラー:', e));
