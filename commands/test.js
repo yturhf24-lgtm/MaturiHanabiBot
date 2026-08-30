cad87b9f86816bbcab252f057b08539cdd7200d8
祭り実行委員会 <yturhf24@gmail.com>
2026-08-30 17:35:18 +0900

    Update test command description and response message

--- commands/test.js
+++ commands/test.js
@@ -1,12 +1,10 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
-  data: new SlashCommandBuilder().setName('test').setDescription('テストコマンド'),
+  data: new SlashCommandBuilder().setName('test').setDescription('動作確認'),
   async execute(i) {
-    const e = new EmbedBuilder()
-      .setColor('Green')
-      .setTitle('✅ 動いてる！')
-      .setDescription('Botは正常に動作しています！');
-    await i.reply({ embeds: [e] });
+    await i.reply({
+      embeds: [new EmbedBuilder().setColor('Green').setTitle('✅ 成功！').setDescription('Botは正常に動いています！')]
+    });
   }
 };
